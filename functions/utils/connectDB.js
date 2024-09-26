ca8e86064da36f64cd41a75c7af5096897809f5e
Lindsey Jackson <lindseylewisjackson@gmail.com>
2024-09-25 20:03:33 -0700

    removed deprecated mongodb connect options

--- functions/utils/connectDB.js
+++ functions/utils/connectDB.js
@@ -3,8 +3,6 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGODB_DATABASE, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
       serverSelectionTimeoutMS: 5000, // Timeout after 5s instead of 30s
       socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
     });
